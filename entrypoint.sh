7285136943a3a86ada7f397b30b05e516e15dd13
Inery-Admin <office@inery.io>
2025-02-21 10:27:45 +0100

    asd

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,16 +3,16 @@
 echo "🔧 Pokrećem test_chain.py..."
 python3 /opt/local_chain/test_chain.py start
 
-echo "🔧 Pokrećem inery-gui-build iz Package direktorijuma (u pozadini)..."
-cd /opt/Package
-nohup ./inery-gui-build &
+echo "🔧 Pokrećem inery-gui iz Package direktorijuma (u pozadini)..."
+cd /opt/local_chain/Packages
+nohup ./inery-gui &
 
 # Sačekaj da se `nodine` pokrene
 echo "⌛ Čekam da se pokrene nodine..."
-sleep 5
+sleep 2
 
 # Proveri da li `nodine` radi
-while pgrep nodine > /dev/null; do
+while pgrep inery-gui > /dev/null; do
     sleep 5  # ponovo proveri svakih 5 sekundi
 done
 
